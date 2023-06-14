b68b37ba8a6276385d278e2140775d999cb5c28d
Isseki WATABE <oneseki743@gmail.com>
2023-06-14 21:35:51 +0900

    wip

--- index.py
+++ index.py
@@ -1,4 +1,26 @@
-a = int(input())
-b, c = map(int, input().split())
-s = input()
-print(a + b + c, s)
+import math
+from collections import deque
+
+S = input()
+
+dict = {S: 0}
+
+queue = deque([S])
+ans = 0
+
+while len(queue):
+    current = queue.popleft()
+
+    if current == "atcoder":
+        ans = dict.get(current)
+        break
+
+    for i in range(1, 7):
+        next = list(current)
+        next[i], next[i - 1] = next[i - 1], next[i]
+        nextStr = "".join(next)
+        if dict.get(nextStr) is None:
+            queue.append(nextStr)
+            dict[nextStr] = dict.get(current) + 1
+
+print(ans)
